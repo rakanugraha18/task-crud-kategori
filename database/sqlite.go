680559529f3795a18a65c31b4a943a648420653a
rakanugraha18 <rakanugraha2@gmail.com>
2026-01-31 15:48:39 +0700

    update database comment

--- database/sqlite.go
+++ database/sqlite.go
@@ -4,12 +4,13 @@ import (
 	"database/sql"
 	"log"
 
-	_ "modernc.org/sqlite" // SQLite driver
+	_ "modernc.org/sqlite" // using modernc.org/sqlite driver for SQLite becouse it's pure Go implementation and cross-platform no need CGO
 )
 
 func InitDB(dbPath string) (*sql.DB, error) {
 	// Open database connection
 	db, err := sql.Open("sqlite", dbPath)
+	// Handle error
 	if err != nil {
 		return nil, err
 	}
@@ -22,7 +23,7 @@ func InitDB(dbPath string) (*sql.DB, error) {
 	// Set connection pool settings
 	db.SetMaxOpenConns(25)
 	db.SetMaxIdleConns(5)
-
+	// Log successful connection
 	log.Println("Database connected successfully")
 	return db, nil
 }
